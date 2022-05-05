fb35d29505334ba1960ac2f08d411507509b57b9
canozzcan <canozcanstu@gmail.com>
2022-05-05 19:19:21 +0300

    Implement Authentication & User Login - Logout Functionality

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const createSessionConfig = require('./config/session');
 const db = require('./data/database');
 const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
+const checkAuthStatusMiddleware = require('./middlewares/check-auth');
 const authRoutes = require('./routes/auth.routes');
+const productRoutes = require('./routes/products.routes');
+const baseRoutes = require('./routes/base.routes');
 
 const app = express();
 
@@ -24,9 +27,12 @@ app.use(expressSession(sessionConfig));
 app.use(csrf());
 
 app.use(addCsrfTokenMiddleware);
+app.use(checkAuthStatusMiddleware);
 
-// authRoutes middleware functions are evaluated for all incoming requests. 
+//middleware functions are evaluated for all incoming requests. 
+app.use(baseRoutes);
 app.use(authRoutes);
+app.use(productRoutes);
 
 app.use(errorHandlerMiddleware);
 
