660b167122ea38598aaeece210333086ae6a45b6
canozzcan <canozcanstu@gmail.com>
2022-05-05 22:57:18 +0300

    Add User Input Validation & Display Error Messages

---
+++ util/validation.js
@@ -0,0 +1,31 @@
+function isEmpty(value) {
+    return !value || value.trim() === '';
+}
+
+function userCrediantial(email, password) {
+    return (
+        email &&
+        email.includes('@') &&
+        password &&
+        password.trim().length >= 6
+    );
+}
+
+function userDetailsAreValid(email, password, name, street, postal, city) {
+    return (
+        userCrediantial(email, password) &&
+        !isEmpty(name) &&
+        !isEmpty(street) &&
+        !isEmpty(postal) &&
+        !isEmpty(city)
+    );
+}
+
+function emailIsConfirmed(email, confirmEmail) {
+    return email === confirmEmail;
+}
+
+module.exports = {
+    userDetailsAreValid: userDetailsAreValid,
+    emailIsConfirmed: emailIsConfirmed
+};
