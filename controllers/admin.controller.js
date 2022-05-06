efff273ced46e1e54e65dd59aab66028d639d397
canozzcan <canozcanstu@gmail.com>
2022-05-06 15:27:09 +0300

    Add Product Admin Pages & Forms

---
+++ controllers/admin.controller.js
@@ -0,0 +1,21 @@
+function getProducts(req, res) {
+    res.render('admin/products/all-products');
+}
+
+function getNewProduct(req, res) {
+    res.render('admin/products/new-product');
+}
+
+function createNewProducts(req, res) {
+    console.log(req.body);
+    console.log(req.file);
+
+    res.redirect('/admin/products');
+}
+
+
+module.exports = {
+    getProducts: getProducts,
+    getNewProduct: getNewProduct,
+    createNewProducts: createNewProducts
+}
